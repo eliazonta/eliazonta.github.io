f8969eeb1344ce712920267dab396a90e672526d
eliazonta <78420183+eliazonta@users.noreply.github.com>
2025-04-13 13:05:38 +0000

    Deploying to gh-pages from @ eliazonta/eliazonta.github.io@5964a8d475d236cc3ac271fcc3ce6494eeed7897 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -146,11 +146,11 @@ ninja.data = [{
           window.open("mailto:%65%6C%69%61.%7A%6F%6E%74%61%61@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/eliazonta", "_blank");
         },
       },{
         id: 'social-rss',
@@ -160,18 +160,11 @@ ninja.data = [{
           window.open("/feed.xml", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
+        id: 'social-x',
+        title: 'X',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://twitter.com/happyeliia", "_blank");
         },
       },{
       id: 'light-theme',
